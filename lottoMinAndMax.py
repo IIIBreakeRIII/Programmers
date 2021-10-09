a919689efcec7b0a40f2c7c84d0a28488c92437e
IIIBreakeRIII <89850286+IIIBreakeRIII@users.noreply.github.com>
2021-10-09 15:48:40 +0900

    lotto Min and Max

---
+++ lottoMinAndMax.py
@@ -0,0 +1,21 @@
+def solution(lottos, win_nums):
+    answer = []
+    rank = [6, 6, 5, 4, 3, 2, 1]
+    #맞힌 개수
+    count = 0
+    
+    for num in lottos:
+        if num in win_nums:
+            count += 1
+    
+    answer.append(rank[count])
+    
+    for num in lottos:
+        if num == 0:
+            count = count + 1
+            
+    answer.append(rank[count])
+    
+    answer.sort()
+            
+    return answer
