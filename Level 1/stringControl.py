665463b18bece053f3c1feef6100d749fd508a55
IIIBreakeRIII <ryu990305@icloud.com>
2023-07-26 16:19:20 +0900

    문자열 다루기 기본

--- Level 1/stringControl.py
+++ Level 1/stringControl.py
@@ -5,14 +5,14 @@ import string
 def solution(s):
     string_list = list(map(str, str(s)))
     for i in range(len(string_list)):
-
-        if len(string_list) >= 4 or len(string_list) <= 6:
+        if len(string_list) == 4 or len(string_list) == 6:
             if string_list[i] in string.ascii_letters:
                 return False
-            else:
-                return True
         else:
-            return True
+            return False
+    
+    return True
 
 print(solution("a234"))
 print(solution("123"))
+print(solution("1234"))
