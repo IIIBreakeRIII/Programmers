5f2d13cf82465adbb67e6f968c00fec6b514c673
IIIBreakeRIII <ryu990305@icloud.com>
2023-08-06 15:44:26 +0900

    두개 뽑아서 더하기

---
+++ Level 1/selectTwoSum.py
@@ -0,0 +1,15 @@
+# 두 개 뽑아서 더하기
+
+def solution(numbers):
+    answer = []
+    for i in range(len(numbers)):
+        for j in range(i + 1, len(numbers)):
+            answer.append(numbers[i] + numbers[j])
+
+    answer = list(set(answer))
+    answer.sort()
+
+    return answer
+
+print(solution([2, 1, 3, 4, 1]))
+print(solution([5, 0, 2, 7]))
