6992695b06982d9a397ad82df00f354c48238088
IIIBreakeRIII <ryu990305@icloud.com>
2023-08-21 17:27:08 +0900

    최솟값 만들기

---
+++ Level 2/minimum.py
@@ -0,0 +1,15 @@
+# 최솟값 만들기
+
+def solution(A, B):
+    answer = 0
+
+    A.sort()
+    B.sort(reverse=True)
+
+    for i in range(len(A)):
+        answer += A[i] * B[i]
+
+    return answer
+
+print(solution([1, 4, 2], [5, 4, 4]))
+print(solution([1, 2], [3, 4]))
