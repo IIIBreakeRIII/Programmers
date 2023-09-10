fd2572bcfe6b28072cda304a5f0c06e9697fe92b
IIIBreakeRIII <ryu990305@icloud.com>
2023-09-10 20:18:08 +0900

    예상 대진표

---
+++ Level 2/tournament.py
@@ -0,0 +1,13 @@
+# 예상 대진표
+
+def solution(n, a, b):
+    answer = 0
+
+    while a != b:
+        answer += 1
+        a = (a + 1) // 2
+        b = (b + 1) // 2
+        
+    return answer
+
+print(solution(8, 4, 7))
