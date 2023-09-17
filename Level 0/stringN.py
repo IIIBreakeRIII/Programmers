8871deb85b5b32e2734b163e2a3e1862e74392f6
IIIBreakeRIII <ryu990305@icloud.com>
2023-09-17 20:30:24 +0900

    SYNC

---
+++ Level 0/stringN.py
@@ -0,0 +1,4 @@
+# 문자열의 앞의 n글자
+
+def solution(my_string, n):
+    return my_string[0:n]
---
+++ Level 0/stringN2.py
@@ -0,0 +1,4 @@
+# 문자열 뒤의 n글자
+
+def solution(my_string, n):
+    return my_string[len(my_string) - n:len(my_string)]
