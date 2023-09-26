7aa4936ff516aa6777a1aff6561e2e1820b16577
IIIBreakeRIII <ryu990305@icloud.com>
2023-09-26 10:37:30 +0900

    SYNC

---
+++ Level 0/dice1.py
@@ -0,0 +1,10 @@
+# 주사위 게임1
+
+def solution(a, b):
+    
+    if a % 2 == 1 and b % 2 == 1:
+        return a ** 2 + b ** 2
+    elif a % 2 == 1 or b % 2 == 1:
+        return 2 * (a + b)
+    else:
+        return abs(a - b)
