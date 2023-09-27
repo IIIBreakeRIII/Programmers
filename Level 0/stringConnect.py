dc3774ed1834aef870a80881d983c88a6b061c29
IIIBreakeRIII <ryu990305@icloud.com>
2023-09-27 12:00:00 +0900

    글자 이어 붙여 문자 만들기

---
+++ Level 0/stringConnect.py
@@ -0,0 +1,10 @@
+# 글자 이어 붙여 문자 만들기
+
+def solution(my_string, index_list):
+    
+    answer = ''
+
+    for i in range(len(index_list)):
+        answer += my_string[index_list[i]]
+
+    return answer
