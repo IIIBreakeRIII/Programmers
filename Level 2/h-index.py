3f6788dc8e4f1dae2240bf1a5c1df3c117ecb846
IIIBreakeRIII <ryu990305@icloud.com>
2023-09-28 15:30:32 +0900

    H-Index

--- Level 2/h-index.py
+++ Level 2/h-index.py
@@ -16,3 +16,19 @@ def solution(citations):
     return max(answer)
 
 print(solution([3, 0, 6, 1, 5]))
+
+def solution2(citations):
+
+    citations.sort(reverse=True)
+    print(citations)
+
+    for idx in range(len(citations)):
+        if idx >= citations[idx]:
+            return idx
+
+    return len(citations)
+
+print(solution2([3, 0, 6, 1, 5]))
+print(solution2([2, 0, 6, 1, 5]))
+print(solution2([0, 0, 0, 0, 0]))
+print(solution2([6, 5, 5, 5, 3, 2, 1, 0]))
