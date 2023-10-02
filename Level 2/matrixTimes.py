e247e11615199cdac5f2986a9d138aa5271022b1
IIIBreakeRIII <ryu990305@icloud.com>
2023-10-02 17:02:29 +0900

    행렬의 곱셈

---
+++ Level 2/matrixTimes.py
@@ -0,0 +1,8 @@
+# 행렬의 곱셈
+
+def solution(arr1, arr2):
+    answer = [[]]
+    return answer
+
+print(solution([[1, 4], [3, 2], [4, 1]], [[3, 3], [3, 3]]))
+print(solution([[2, 3, 2], [4, 2, 4], [3, 1, 4]], [[5, 4, 3], [2, 4, 1], [3, 1, 1]]))
