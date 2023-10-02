6285abf93674e200bc27d419ca10be679d164dee
IIIBreakeRIII <ryu990305@icloud.com>
2023-10-02 17:54:17 +0900

    행렬의 곱셈

--- Level 2/matrixTimes.py
+++ Level 2/matrixTimes.py
@@ -1,7 +1,14 @@
 # 행렬의 곱셈
 
 def solution(arr1, arr2):
-    answer = [[]]
+
+    answer = [[0 for _ in range(len(arr2[0]))] for _ in range(len(arr1))]
+
+    for i in range(len(arr1)): 
+        for j in range(len(arr2[0])): 
+            for k in range(len(arr1[0])): 
+                answer[i][j] += arr1[i][k] * arr2[k][j]
+
     return answer
 
 print(solution([[1, 4], [3, 2], [4, 1]], [[3, 3], [3, 3]]))
