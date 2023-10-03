0f3cfaa8a63e69072462763f5df9c6228bb64b06
IIIBreakeRIII <ryu990305@icloud.com>
2023-10-03 16:50:32 +0900

    SYNC

---
+++ Level 0/blank.py
@@ -0,0 +1,6 @@
+# 공백으로 구분하기
+
+def solution(my_string):
+    return my_string.split()
+
+print(solution("i    love you"))
---
+++ Level 0/makeArray.py
@@ -0,0 +1,4 @@
+# 배열 만들기
+
+def solution(n, k):
+    return [i for i in range(k, n + 1, k)]
