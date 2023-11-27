f68a201a48fa0d800507400f17be3e7c023af073
IIIBreakeRIII <ryu990305@icloud.com>
2023-11-27 17:16:06 +0900

    덧칠하기

---
+++ Level 1/painting.py
@@ -0,0 +1,13 @@
+# 덧칠하기
+
+def solution(n, m, section):
+    
+    answer = 1
+    paint = section[0]
+    
+    for i in range(1, len(section)):
+        if section[i] - paint >= m:
+            answer += 1
+            paint = section[i]
+            
+    return answer
